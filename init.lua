5bad318efd13418628ea4e62a88bcd29d0386df2
codemdg <rivoherizo2003@gmail.com>
2024-10-18 17:16:20 +0300

    add config phpactor for specific project

--- init.lua
+++ init.lua
@@ -14,8 +14,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
-
+vim.opt.relativenumber = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
@@ -409,6 +408,12 @@ require('lazy').setup({
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
       --    function will be executed to configure the current buffer
+      --
+      local project_config = vim.fn.getcwd() .. '/.nvim.lua'
+      if vim.fn.filereadable(project_config) == 1 then
+        dofile(project_config)
+      end
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
